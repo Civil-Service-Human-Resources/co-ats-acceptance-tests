bc991b9156605c12be9efcca5c0b992ce6b7b9a2
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2023-11-19 15:32:42 +0000

    Add asserts to spec

--- src/test/scala/uk/gov/co/test/ui/specs/v9/ApplicationSpec.scala
+++ src/test/scala/uk/gov/co/test/ui/specs/v9/ApplicationSpec.scala
@@ -1,10 +1,11 @@
 package uk.gov.co.test.ui.specs.v9
 
+import org.openqa.selenium.By
 import uk.gov.co.test.ui.data.v9.{CANDIDATE_SHORT_FORM_DATA, REGISTERED_CANDIDATE, REGISTER_CANDIDATE}
 import uk.gov.co.test.ui.flows.v9.RegisterCandidateFlow.fillNewCandidateDetails
 import uk.gov.co.test.ui.flows.v9.ShortFormFlow.fillShortFormDetails
 import uk.gov.co.test.ui.pages.v9.SearchJobsPage.{candidateDisplayName, navigateToSignInOrCreateAccount}
-import uk.gov.co.test.ui.pages.v9.SignInPage.{candidateFullName, candidateSignIn}
+import uk.gov.co.test.ui.pages.v9.SignInPage.{candidateFullName, candidateSignIn, onPage}
 import uk.gov.co.test.ui.specs.BaseFeatureSpec
 import uk.gov.co.test.ui.tags.RunInV9
 
@@ -30,6 +31,10 @@ class ApplicationSpec extends BaseFeatureSpec {
       fillShortFormDetails(CANDIDATE_SHORT_FORM_DATA)
 
       Then("The candidate is able to see their account")
+      eventually(onPage("Application Centre - Civil Service Jobs - GOV.UK"))
+      driver.findElement(By.xpath(".//input[@value='Advert Details']")).isDisplayed
+      driver.findElement(By.xpath(".//input[@value='Withdraw Application']")).isDisplayed
+      driver.findElement(By.tagName("b")).getText shouldEqual "Help with selection process"
     }
   }
 }
