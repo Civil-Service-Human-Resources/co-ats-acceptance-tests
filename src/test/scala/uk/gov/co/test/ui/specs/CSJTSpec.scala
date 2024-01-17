78c87d81ec82f104c54906e3b42cd49e774d2c37
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-01-17 22:24:25 +0000

    WIP2-Add new CSJT spec

--- src/test/scala/uk/gov/co/test/ui/specs/CSJTSpec.scala
+++ src/test/scala/uk/gov/co/test/ui/specs/CSJTSpec.scala
@@ -4,10 +4,12 @@ import uk.gov.co.test.ui.data.v9.applicants._
 import uk.gov.co.test.ui.data.v9.longform.MASTER_LONG_FORM_DATA
 import uk.gov.co.test.ui.data.v9.shortform.MASTER_SHORT_FORM_DATA
 import uk.gov.co.test.ui.data.vx.CSJT_VACANCY_DATA
+import uk.gov.co.test.ui.data.vx.MasterVacancyDetails.{vXAnyOnlineTests, vacancyId}
 import uk.gov.co.test.ui.flows.v9.LongFormFlow.fillLongFormDetails
 import uk.gov.co.test.ui.flows.v9.RegisterCandidateFlow.fillNewCandidateDetails
 import uk.gov.co.test.ui.flows.v9.ShortFormFlow.fillShortFormDetails
 import uk.gov.co.test.ui.flows.vx.NewVacancyFlow.fillNewVacancyForm
+import uk.gov.co.test.ui.pages.vx.VacancyDetailsPage.extractAllVacancyDetails
 import uk.gov.co.test.ui.tags.RunInVX
 
 class CSJTSpec extends BaseFeatureSpec {
@@ -15,6 +17,20 @@ class CSJTSpec extends BaseFeatureSpec {
     Scenario("VX: Recruiter Creates A Master Vacancy With One CSJT Online", RunInVX) {
       Given("a recruiter creates & posts a new vacancy with csjt")
       fillNewVacancyForm(CSJT_VACANCY_DATA)
+
+      When("candidate registers for new job application with csjt required")
+      fillNewCandidateDetails(MASTER_REGISTER_CANDIDATE)
+      fillShortFormDetails(MASTER_SHORT_FORM_DATA)
+      fillLongFormDetails(MASTER_LONG_FORM_DATA)
+
+      Then("the candidate is able to complete the csjt")
+    }
+
+    Scenario("VX: Candidate Applies For Vacancy With One CSJT Online", RunInVX) {
+      Given("vacancy details are extracted and values set")
+      vacancyId = "9604"
+      vXAnyOnlineTests = true
+      extractAllVacancyDetails(vacancyId)
 
       When("candidate registers for new job application with csjt required")
       fillNewCandidateDetails(MASTER_REGISTER_CANDIDATE)
