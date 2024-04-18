6c3b5d7aff9b6e6fae3f5652b61335f89006b52d
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-04-18 20:59:47 +0100

    PEC Employment History Page

---
+++ src/test/scala/uk/gov/co/test/ui/data/test/pec/PecForm.scala
@@ -0,0 +1,89 @@
+package uk.gov.co.test.ui.data.test.pec
+
+import uk.gov.co.test.ui.data.v9.pecform.PecFormDetails
+import uk.gov.co.test.ui.pages.v9.pecform._
+import uk.gov.co.test.ui.pages.vx.DashboardPage.v9RefereeEmail
+
+object PEC_FORM_DATA
+    extends PecFormDetails(
+      PEC_FORM_YOUR_DETAILS,
+      PEC_RIGHT_TO_WORK,
+      PEC_FORM_EMPLOYMENT_HISTORY,
+      PEC_FORM_VERIFYING_HISTORY,
+      PEC_FORM_DECLARATION
+    )
+
+object PEC_FORM_YOUR_DETAILS
+    extends YourDetails(
+      "Mr",
+      true,
+      "JA123456D",
+      "Autotest - Provide details on your nino application status",
+      "01/01/1980",
+      "Man",
+      "Single",
+      "1001 Autotest street",
+      addressLineTwo = Option("Cheadle"),
+      "Manchester",
+      "Cheshire",
+      "United Kingdom",
+      "SK91BX",
+      "00110000001"
+    )
+
+object PEC_RIGHT_TO_WORK
+    extends RtwDetails(
+      "British citizen, Briton",
+      "British citizen, Briton",
+      true,
+      "Egyptian",
+      false,
+      true,
+      true,
+      false,
+      "I have applied for the settlement scheme and await confirmation of my status",
+      "Autotest-certificate/application number",
+      "Autotest-shareCode",
+      true,
+      "Autotest- 'View and Prove' code",
+      "Test-T&Cs.pdf",
+      true,
+      true,
+      true
+    )
+
+object PEC_FORM_EMPLOYMENT_HISTORY
+    extends EmploymentHistoryDetails(
+      true,
+      "Employer Test One",
+      "01/01/2023",
+      "01/12/2023",
+      "Employer Test Two",
+      "01/07/2021",
+      "31/12/2022",
+      "Employer Test Three",
+      "01/01/2020",
+      "01/06/2021"
+    )
+
+object PEC_FORM_VERIFYING_HISTORY
+    extends VerifyingHistoryDetails(
+      true,
+      "Autotest1 - relation to referee",
+      "Employer",
+      "Employer1 Test1",
+      s"$v9RefereeEmail",
+      "Academic",
+      "Autotest2 - relation to referee",
+      "Academic2 Test2",
+      s"$v9RefereeEmail",
+      "Other",
+      "Autotest3 - relation to referee",
+      "Other3 Test3",
+      s"$v9RefereeEmail"
+    )
+
+object PEC_FORM_DECLARATION
+    extends DeclarationDetails(
+      true
+    )
--- src/test/scala/uk/gov/co/test/ui/specs/PecVacancySpec.scala
+++ src/test/scala/uk/gov/co/test/ui/specs/PecVacancySpec.scala
@@ -1,8 +1,7 @@
 package uk.gov.co.test.ui.specs
 
-import uk.gov.co.test.ui.data.test.pec.{PEC_APPLICATION_DATA, PEC_LONG_FORM_DATA, PEC_SHORT_FORM_DATA, PEC_VACANCY_DATA}
+import uk.gov.co.test.ui.data.test.pec._
 import uk.gov.co.test.ui.data.v9.applicants.REGISTER_CANDIDATE_PEC
-import uk.gov.co.test.ui.data.v9.pecform.MASTER_PEC_FORM_DATA
 import uk.gov.co.test.ui.flows.e2e.InterviewFlow.completeAllInterviews
 import uk.gov.co.test.ui.flows.v9.LongFormFlow.fillLongFormDetails
 import uk.gov.co.test.ui.flows.v9.PecFormFlow.fillPecFormDetailsOnly
@@ -10,7 +9,6 @@ import uk.gov.co.test.ui.flows.v9.RegisterCandidateFlow.fillNewCandidateDetails
 import uk.gov.co.test.ui.flows.v9.ShortFormFlow.fillShortFormDetails
 import uk.gov.co.test.ui.flows.vx.NewVacancyFlow.fillNewVacancyForm
 import uk.gov.co.test.ui.pages.vx.ApplicationSummaryPage.moveAndAcceptOffer
-import uk.gov.co.test.ui.pages.vx.VacancyDetailsPage.extractAllVacancyDetails
 import uk.gov.co.test.ui.pages.vx.vacancytabs.PreSiftEvaluationTab.PreSiftEvaluationFlow
 import uk.gov.co.test.ui.pages.vx.vacancytabs.SiftEvaluationTab.SiftEvaluationFlow
 import uk.gov.co.test.ui.tags.RunInVX
@@ -34,7 +32,7 @@ class PecVacancySpec extends BaseFeatureSpec {
       moveAndAcceptOffer()
 
       Then("the candidate is able to fully complete the pec form")
-      fillPecFormDetailsOnly(MASTER_PEC_FORM_DATA)
+      fillPecFormDetailsOnly(PEC_FORM_DATA)
     }
   }
 }
