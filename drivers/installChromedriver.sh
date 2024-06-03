bdee0a948963b74ef4b4e4476a13a6bc2cbf86f5
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-06-03 14:06:20 +0100

    update chromedriver execute file

--- drivers/installChromedriver.sh
+++ drivers/installChromedriver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#This script supports linux and mac/macM1 installations.
+#This script supports linux and mac-x64/mac-arm64 installations.
 OS=${1:-mac-arm64}
 
 die () {
@@ -9,11 +9,9 @@ die () {
 
 [ "$OS" == "linux64" ] || [ "$OS" == "mac-x64" ] ||  [ "$OS" == "mac-arm64" ] die "First argument must be one of [linux64,mac-x64,mac-arm64].  \"$OS\" is not supported"
 
-#CHROME_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
 CHROME_VERSION=`curl -sS  https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE`
 CHROME_PARENT_DIR="/usr/local/bin/"
 
-#CHROME_DOWNLOAD_URL="http://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_${OS}.zip"
 CHROME_DOWNLOAD_URL="https://storage.googleapis.com/chrome-for-testing-public/$CHROME_VERSION/${OS}/chromedriver-${OS}.zip"
 CHROME_TEMP="/tmp/chromedriver-v${CHROME_VERSION}-${OS}.tar.gz"
 
