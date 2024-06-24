e23e0d61f27237c0f6caf23f2c630e315fc43db9
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-06-24 19:21:58 +0100

    interview forms 1 & 2 eval complete

--- src/test/scala/uk/gov/co/test/ui/data/vx/vacancy/MasterVacancy.scala
+++ src/test/scala/uk/gov/co/test/ui/data/vx/vacancy/MasterVacancy.scala
@@ -477,12 +477,12 @@ object MASTER_PEC_CHECK_FORMS
       selfEmploymentCheck =
         ListBuffer("Internal Candidates", "External Candidates", "OGD Candidates", "NDPB Candidates"),
       true,
-      true,
+      false,
       true,
       "Autotest - Name of check",
-      additionalCheck = ListBuffer("Not Applicable"),
-      nenOnboarding = ListBuffer("Internal Candidates", "External Candidates", "OGD Candidates", "NDPB Candidates"),
+      additionalCheck = ListBuffer("Internal Candidates", "External Candidates", "OGD Candidates", "NDPB Candidates"),
+      nenOnboarding = ListBuffer("External Candidates", "OGD Candidates", "NDPB Candidates"),
       s"$contactEmailVxConfig",
-      pnOnboarding = ListBuffer("Internal Candidates", "External Candidates", "OGD Candidates", "NDPB Candidates"),
+      pnOnboarding = ListBuffer("Internal Candidates"),
       s"$contactEmailVxConfig"
     )
