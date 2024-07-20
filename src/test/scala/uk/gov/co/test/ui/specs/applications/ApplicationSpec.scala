cba5ef4267275327b7419c0233cfbf93e66a1172
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-07-20 02:11:33 +0100

    IDVT full logic

--- src/test/scala/uk/gov/co/test/ui/specs/applications/ApplicationSpec.scala
+++ src/test/scala/uk/gov/co/test/ui/specs/applications/ApplicationSpec.scala
@@ -19,7 +19,7 @@ class ApplicationSpec extends BaseFeatureSpec {
     Scenario("VX: All Forms; Master Application Process", RunInVX) {
       Given("new vacancy is created and posted")
       fillNewVacancyForm(MASTER_VACANCY_DATA)
-//      extractAllVacancyDetails("10312")
+//      extractAllVacancyDetails("10407")
 
       When("recruiter & new candidate complete the full application")
       fillNewCandidateDetails(MASTER_REGISTER_CANDIDATE)
