cdd78a6bc58f89f0fb25a73e5539ca7ba0e230b7
Gulam Choudhury <148063814+gchoudhury7@users.noreply.github.com>
2024-07-26 12:09:23 +0100

    Redeployment logic

--- src/test/scala/uk/gov/co/test/ui/specs/applications/PnNenSpec.scala
+++ src/test/scala/uk/gov/co/test/ui/specs/applications/PnNenSpec.scala
@@ -1,14 +1,12 @@
 package uk.gov.co.test.ui.specs.applications
 
 import uk.gov.co.test.ui.data.MasterVacancyDetails.{v9HomeDepartment, vXJobInfoDepartment}
-import uk.gov.co.test.ui.data.test.pnnen.PN_NEN_VACANCY_DATA
 import uk.gov.co.test.ui.data.v9.applicants.{REGISTER_CANDIDATE_NEN_DEFRA, REGISTER_CANDIDATE_PN_DEFRA}
 import uk.gov.co.test.ui.data.v9.longform.MASTER_LONG_FORM_DATA
 import uk.gov.co.test.ui.data.v9.shortform.MASTER_SHORT_FORM_DATA
 import uk.gov.co.test.ui.data.vx.application.MASTER_APPLICATION_DATA
 import uk.gov.co.test.ui.flows.e2e.FullApplicationFlow.nenPnFlow
 import uk.gov.co.test.ui.flows.v9.RegisterCandidateFlow.fillNewCandidateDetails
-import uk.gov.co.test.ui.flows.vx.NewVacancyFlow.fillNewVacancyForm
 import uk.gov.co.test.ui.pages.v9.ApplicationCentrePage.{confirmApplicationUpdateState, passedPecChecks}
 import uk.gov.co.test.ui.pages.vx.ApplicationSummaryPage.{onboardingNenComplete, postingNoticeCompleted}
 import uk.gov.co.test.ui.pages.vx.VacancyDetailsPage.extractAllVacancyDetails
@@ -21,8 +19,8 @@ class PnNenSpec extends BaseFeatureSpec {
   Feature("Recruiter Completes The PN Form") {
     Scenario("VX: A Recruiter Completes The PN Form (DEFRA)", RunInVX) {
       Given("candidate registers for new job application")
-      fillNewVacancyForm(PN_NEN_VACANCY_DATA)
-//      extractAllVacancyDetails("10438")
+//      fillNewVacancyForm(PN_NEN_VACANCY_DATA)
+      extractAllVacancyDetails("10440")
       fillNewCandidateDetails(REGISTER_CANDIDATE_PN_DEFRA)
 
       When("internal candidate completes all forms until pn form")
